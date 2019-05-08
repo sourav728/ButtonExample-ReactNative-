d6eeb3b27149f346712b0e462c112bea17867616
sourav728 <souravnandi728@outlook.com>
2019-05-08 18:02:45 +0530

    login signup screen created

--- App.js
+++ App.js
@@ -1,57 +1,103 @@
 
 
 import React, {Component} from 'react';
-import {Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
+import {
+  StyleSheet,Text,View,TextInput,Button,TouchableHighlight,Image,Alert
+} from 'react-native';
 
 export default class App extends Component {
-  onPressButton(){
-    Alert.alert("Button clicked!!")
+    constructor(props){
+      super(props);
+      state = {
+        email : '',
+        password : '',
+      }
+
+    }
+    onClickListener = (viewId) =>{
+      Alert.alert("Alert", "Button pressed "+viewId);
+    }
+
+    render() {
+      return (
+        <View style={styles.container}>
+          <View style={styles.inputContainer}>
+            <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/message/ultraviolet/50/3498db'}}/>
+            <TextInput style={styles.inputs}
+                placeholder="Email"
+                keyboardType="email-address"
+                underlineColorAndroid='transparent'
+                onChangeText={(email) => this.setState({email})}/>
+          </View>
+          
+          <View style={styles.inputContainer}>
+            <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/key-2/ultraviolet/50/3498db'}}/>
+            <TextInput style={styles.inputs}
+                placeholder="Password"
+                secureTextEntry={true}
+                underlineColorAndroid='transparent'
+                onChangeText={(password) => this.setState({password})}/>
+          </View>
+  
+          <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} onPress={() => this.onClickListener('login')}>
+            <Text style={styles.loginText}>Login</Text>
+          </TouchableHighlight>
+  
+          <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('restore_password')}>
+              <Text>Forgot your password?</Text>
+          </TouchableHighlight>
+  
+          <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('register')}>
+              <Text>Register</Text>
+          </TouchableHighlight>
+        </View>
+      );
+    }
   }
-  render() {
-    return (
-      <View style={styles.container}>  
-                <View style={styles.buttonContainer}>  
-                    <Button  
-                        onPress={this.onPressButton}  
-                        title="Click Me"  
-                    />  
-                </View>  
-
-                <View style = {styles.buttonContainer}>
-                <Button  
-                        onPress={this.onPressButton}  
-                        title="Click Me"  
-                    />  
-                </View>
-
-                <View style={styles.multiButtonContainer}>  
-                    <Button  
-                        onPress={this.onPressButton}  
-                        title="A disabled button"  
-                        disabled={true}  
-                    />  
-                    <Button  
-                        onPress={this.onPressButton}  
-                        title="OK!"  
-                        color="#009933"  
-                    />  
-                </View>  
-            </View>  
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: { 
-    flex: 1,
-    justifyContent: 'center',
-  },
- buttonContainer:{
-   margin: 20
- },
- multiButtonContainer:{
-   margin: 20,
-   flexDirection: 'row',
-   justifyContent: 'space-between'
- },
-});
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#DCDCDC',
+    },
+    inputContainer: {
+        borderBottomColor: '#F5FCFF',
+        backgroundColor: '#FFD54F',
+        borderRadius:30,
+        borderBottomWidth: 1,
+        width:250,
+        height:45,
+        marginBottom:20,
+        flexDirection: 'row',
+        alignItems:'center'
+    },
+    inputs:{
+        height:45,
+        marginLeft:16,
+        borderBottomColor: '#FFFFFF',
+        flex:1,
+    },
+    inputIcon:{
+      width:30,
+      height:30,
+      marginLeft:15,
+      justifyContent: 'center'
+    },
+    buttonContainer: {
+      height:45,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginBottom:20,
+      width:250,
+      borderRadius:30,
+    },
+    loginButton: {
+      backgroundColor: "#8D6E63",
+    },
+    loginText: {
+      color: 'white',
+    }
+  });
